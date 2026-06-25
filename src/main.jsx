041bf0c1b0d9afcc7cb8157732bb68cd45f7644d
Javier Sebastian <u20211e429@upc.edu.pe>
2026-06-25 16:59:40 -0500

    fix: firebase deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-console.log('App loaded - v2');
+console.log('App loaded - v3');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
